4ddbbf0c82460b4eb54a646edd392c606fb7662f
wxwcase <weicwru@gmail.com>
2015-02-10 12:28:49 -0500

    [Added Excel Sheet Column Number]

---
+++ src/info/test/HasGlobalTimeout.java
@@ -0,0 +1,28 @@
+package info.test;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class HasGlobalTimeout {
+	public static String log;
+	
+	@Rule
+	public Timeout globalTimeout = new Timeout(3000); // 3 seconds max per method
+	
+	@Test
+	public void testInfiniteLoop1() {
+		log += "ran1";
+		for(;;) {
+			
+		}
+	}
+	
+	@Test 
+	public void testInfiniteLoop2() {
+		log += "ran2";
+		for(;;) {
+			
+		}
+	}
+}
