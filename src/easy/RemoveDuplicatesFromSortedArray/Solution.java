b946c4dbdcbfe28580cac1b64b8c8ecc0efe433e
wxwcase <weicwru@gmail.com>
2015-02-19 16:36:12 -0500

    [Remove Duplicates from Sorted Array]

---
+++ src/easy/RemoveDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,41 @@
+package easy.RemoveDuplicatesFromSortedArray;
+
+public class Solution {
+	
+	/**
+	 * Given an sorted array, remove the duplicates in place such that each element
+	 * appear only once and return the new length
+	 * Do not allocate extra space for another array, do this inplace with constant memory
+	 * @param A
+	 * @return
+	 */
+	public int removeDuplicates(int[] A) {
+		// Algorithm:
+		// 1. loop through the array
+		//		- if find duplicate, shift the elements on the right to left by one
+		//		- keep counting how many elements deleted
+		if (A == null) {
+			return 0;
+		} 
+		int newLen = A.length;
+		if (newLen < 2) {
+			return newLen;
+		}
+		
+		int i = 0, j = 1;
+		while(j < A.length) {
+			int count = 0;
+			while (j < A.length && A[i] == A[j]) {
+				j++;
+				count++;
+			}
+			if (j < A.length) {
+				A[i + 1] = A[j];
+			}
+			j++;
+			i++;
+			newLen -= count;
+		}
+		return newLen;
+	}
+}
