41534c4815af968aff42dc25049d1c9f83b63da8
wxwcase <weicwru@gmail.com>
2015-02-23 11:48:13 -0500

    [Repeated DNA Sequences]

--- src/medium/repeateddnasequences/Solution.java
+++ src/medium/repeateddnasequences/Solution.java
@@ -2,10 +2,23 @@ package medium.repeateddnasequences;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Solution {
+  
+  private static final Map<Character, Integer> MAP = new HashMap<>();
+
+  static {
+    MAP.put('A', 0);
+    MAP.put('C', 1);
+    MAP.put('G', 2);
+    MAP.put('T', 3);
+  }
+
+  private static final int SIZE_POW_9 = (int)Math.pow(MAP.size(), 9);
 
   /**
    * Find all the 10-letter-long sequences(substrings) that occur more than once in a 
@@ -13,8 +26,18 @@ public class Solution {
    * @param str input DNA sequence 
    * @return list of repeated sequence
    */
-  public List<String> findRepeatedDnaSequence(String str) {
-    List<String> result = new ArrayList<>();
-    return result;
+  public List<String> findRepeatedDnaSequences(String str) {
+    Set<String> result = new HashSet<>();
+    Set<Integer> hashes = new HashSet<>();
+    for (int i = 0, hashVal = 0; i < str.length(); i++) {
+      if (i > 9) {
+        hashVal -= SIZE_POW_9 * MAP.get(str.charAt(i - 10));
+      } 
+      hashVal = MAP.size() * hashVal + MAP.get(str.charAt(i));
+      if (i > 8 && !hashes.add(hashVal)) {
+        result.add(str.substring(i - 9, i + 1));
+      }
+    }
+    return new ArrayList<>(result);
   }
 }
