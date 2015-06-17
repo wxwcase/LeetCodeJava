cd25507db81482198f8337cdbbe0231c3b999288
wxwcase <weicwru@gmail.com>
2015-06-17 14:51:12 -0400

    [House Robber]

--- src/easy/houserobber/Solution.java
+++ src/easy/houserobber/Solution.java
@@ -3,11 +3,31 @@ package easy.houserobber;
 public class Solution {
 
   /**
-   * 
-   * @param nums
-   * @return
+   * You are a professional robber planning to rob houses 
+   * along a street. Each house has a certain amount of money 
+   * stashed, the only constraint stopping you from robbing 
+   * each of them is that adjacent houses have security system 
+   * connected and it will automatically contact the police if 
+   * two adjacent houses were broken into on the same night. 
+   * Given a list of non-negative integers representing the amount 
+   * of money of each house, determine the maximum amount of money 
+   * you can rob tonight without alerting the police.
+   * @param nums input
+   * @return maximum amount
    */
   public int rob(int[] nums) {
-    return 0;
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    
+    int n = nums.length;
+    int[] dp = new int[n + 1];
+    dp[0] = 0;
+    dp[1] = nums[0];
+    
+    for (int i = 2; i < n + 1; i++) {
+      dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+    }
+    return dp[n];
   }
 }
